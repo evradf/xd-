0d4d5a4bfa658c5373c3164bf2cf4055f8f89609
evradf <99187871+evradf@users.noreply.github.com>
2022-04-18 09:25:57 -0300

    Add files via upload

---
+++ xd/xd/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+export default {
+  components: { MenuLateral },
+};
+</script>
+
+<template>
+  <MenuLateral />
+  <header class="header">Cabeçalho</header>
+  <main class="content">Principal</main>
+  <footer class="footer">Rodapé</footer>
+</template>
+
+<style></style>
---
+++ xd/xd/src/components/MenuLateral.vue
@@ -0,0 +1,17 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <aside class="sidenav">
+    <ul>
+      <li>Item1</li>
+      <li>Item2</li>
+      <li>Item3</li>
+      <li>Item4</li>
+      <li>Item5</li>
+    </ul>
+  </aside>
+</template>
+
+<style></style>
